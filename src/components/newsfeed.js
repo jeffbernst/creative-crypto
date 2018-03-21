4e8895d11dba6d7c6a04cc46896758e5d8d47ba9
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-03-21 16:44:57 -0400

    add twitter feed embed code

--- src/components/newsfeed.js
+++ src/components/newsfeed.js
@@ -5,7 +5,11 @@ import './newsfeed.css';
 export function NewsFeed() {
 	return (
 		<div className="newsfeed">
-			news
+			<a class="twitter-timeline"
+				 data-height="1660"
+				 href="https://twitter.com/creative_crypto?ref_src=twsrc%5Etfw">
+				Tweets by creative_crypto
+			</a>
 		</div>
 	)
 }

53e7afe4b4ff5b4bc7a8b93d73544ac3a990bfa8
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-03-22 17:38:10 -0400

    adjust markdown parser

--- src/components/tag-list.js
+++ src/components/tag-list.js
@@ -5,11 +5,9 @@ import './tag-list.css';
 export function TagList() {
 	return (
 		<section className="top-tag-list">
-			<div className="top-tag top-tag-1">NEWS</div>
-			<div className="top-tag top-tag-2">ANALYSIS</div>
-			<div className="top-tag top-tag-3">ART</div>
-			<div className="top-tag top-tag-4">DESIGN</div>
-			<div className="top-tag top-tag-5">RESOURCE</div>
+			<div className="top-tag top-tag-3">HOME</div>
+			<div className="top-tag top-tag-4">RESOURCES</div>
+			<div className="top-tag top-tag-5">ABOUT</div>
 		</section>
 	)
 }

ba6d75063783fbf03d2e9a50a0287ed595988a78
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-03-22 15:16:59 -0400

    add more styling for small post feed tile

--- src/components/post-feed-small-tile.js
+++ src/components/post-feed-small-tile.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
+import './post-feed-small-tile.css';
+
 export function PostFeedSmallTile(props) {
+	const tagArray = props.tags.map(tag => (
+		<div className="post-tile-tag">{tag}</div>
+	));
+
 	return (
 		<div className="post-tile small-post-tile">
-			<img src={props.image} alt="" />
-				<div className="post-content-preview">
-					<Link to={`/${props.permlink}`}>
-						<div className="post-tile-title">{props.title}</div>
-					</Link>
-					<div className="post-tile-stats">
-						<span className="post-tile-value">{props.pendingPayoutValue}</span>
-						<span className="post-tile-votes">{props.numberOfVotes}</span>
-					</div>
-					<div className="post-tile-tags">{props.tags}</div>
+			<Link to={`/${props.permlink}`}>
+				<div className="image">
+					<img src={props.image} alt=""/>
+					<div><span>stuff</span></div>
 				</div>
+				<div className="post-content-preview">
+					<div className="post-tile-title">{props.title}</div>
+					<div className="post-content-preview-bottom-container">
+						<div className="post-tile-stats">
+							<span className="post-tile-value">${props.pendingPayoutValue}</span>&nbsp;&nbsp;&nbsp;
+							<span className="post-tile-votes">&#x2303;{props.numberOfVotes}</span>
+						</div>
+						<div className="post-tile-tag-list">{tagArray}</div>
+					</div>
+				</div>
+			</Link>
 		</div>
 	)
 }

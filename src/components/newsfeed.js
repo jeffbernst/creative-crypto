0d5127e9976c54f0fe66d331044870c19a8d7e3f
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-03-21 20:32:40 -0400

    remove stock twitter timeline

--- src/components/newsfeed.js
+++ src/components/newsfeed.js
@@ -5,11 +5,7 @@ import './newsfeed.css';
 export function NewsFeed() {
 	return (
 		<div className="newsfeed">
-			<a class="twitter-timeline"
-				 data-height="1660"
-				 href="https://twitter.com/creative_crypto?ref_src=twsrc%5Etfw">
-				Tweets by creative_crypto
-			</a>
+		 newsfeed
 		</div>
 	)
 }

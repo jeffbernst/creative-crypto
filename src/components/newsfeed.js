4c6dfc9b83fa51cd7ec145e40d6954240ac7b70e
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-03-28 15:03:12 -0400

    add stock twitter timeline back into newsfeed

--- src/components/newsfeed.js
+++ src/components/newsfeed.js
@@ -8,29 +8,34 @@ import {Tweet} from './tweet';
 import {getCurrentNewsfeed} from "../actions";
 
 class Newsfeed extends React.Component {
-	componentDidMount() {
-		this.props.getCurrentNewsfeed();
-	}
+	// componentDidMount() {
+	// 	this.props.getCurrentNewsfeed();
+	// }
 
 	render() {
-		if (this.props.loadingNewsfeed) {
-			return <Spinner />;
-		}
-
-		if (this.props.error) {
-			return <strong>{this.props.error}</strong>;
-		}
-
-		const tweets = this.props.newsfeed.map((tweet, index) => {
-			return <Tweet fullText={tweet.fullText} timeSinceTweeted={tweet.timeSinceTweeted} tweetId={tweet.tweetId}/>;
-		});
-
-		const firstTenTweets = tweets.slice(0, 9);
+		// if (this.props.loadingNewsfeed) {
+		// 	return <Spinner />;
+		// }
+		//
+		// if (this.props.error) {
+		// 	return <strong>{this.props.error}</strong>;
+		// }
+		//
+		// const tweets = this.props.newsfeed.map((tweet, index) => {
+		// 	return <Tweet fullText={tweet.fullText} timeSinceTweeted={tweet.timeSinceTweeted} tweetId={tweet.tweetId}/>;
+		// });
+		//
+		// const firstTenTweets = tweets.slice(0, 9);
 
 		return (
 			<div className="newsfeed">
-				<a href="http://twitter.com/creative_crypto"><div className="newsfeed-title-bar">NEWSFEED</div></a>
-				{firstTenTweets}
+				{/*<a href="http://twitter.com/creative_crypto"><div className="newsfeed-title-bar">NEWSFEED</div></a>*/}
+				{/*{firstTenTweets}*/}
+				<a class="twitter-timeline"
+					 data-height="1660"
+					 href="https://twitter.com/creative_crypto?ref_src=twsrc%5Etfw">
+					Tweets by creative_crypto
+				</a>
 			</div>
 		)
 	}
--- src/components/post-feed-small-tile.js
+++ src/components/post-feed-small-tile.js
@@ -19,7 +19,8 @@ export function PostFeedSmallTile(props) {
 					<div><span>{props.tags[0]}</span></div>
 				</div>
 				<div className="post-content-preview">
-					<Shiitake lines={2} className="post-tile-title" >{props.title}</Shiitake>
+					<Shiitake lines={4} className="post-tile-title" >{props.title}</Shiitake>
+					{/*<div className="post-tile-title">{props.title}</div>*/}
 					{/*<Shiitake lines={2} className="post-body-preview">{props.body}</Shiitake>*/}
 					<div className="post-content-preview-bottom-container">
 						<div className="post-tile-stats">

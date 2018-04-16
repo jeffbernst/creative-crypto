61d6939c00dce9a52e0120682adf28d0a86a19e3
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-04-16 14:07:31 -0400

    update nav and shadow colors

--- src/components/tag-list.js
+++ src/components/tag-list.js
@@ -5,9 +5,9 @@ import './tag-list.css';
 export function TagList() {
 	return (
 		<section className="top-tag-list">
-			<div className="top-tag top-tag-3">HOME</div>
-			<div className="top-tag top-tag-4">RESOURCES</div>
-			<div className="top-tag top-tag-5">ABOUT</div>
+			<div className="top-tag top-tag-1">HOME</div>
+			<div className="top-tag top-tag-2">RESOURCES</div>
+			<div className="top-tag top-tag-2">ABOUT</div>
 		</section>
 	)
 }

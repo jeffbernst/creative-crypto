ba15759c8a0e9be5af66e4d89b8ffa0aac4d6912
Jeff Bernstein <jeffreybbernstein@gmail.com>
2018-05-10 15:27:48 -0400

    add korean slides to resources page, enable toggle buttons, add modal

--- src/components/resources.js
+++ src/components/resources.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+// english images
 import blockHeader from '../img/resources/english/Encyclopedia - Block Header Slide.png'
 import blockHeight from '../img/resources/english/Encyclopedia - Block Height Slide.png'
 import block from '../img/resources/english/Encyclopedia - Block Slide.png'
@@ -9,11 +10,23 @@ import mainChain from '../img/resources/english/Encyclopedia - Main Chain Slide.
 import merkleTree from '../img/resources/english/Encyclopedia - Merkle Tree Slide.png'
 import orphanedBlock from '../img/resources/english/Encyclopedia - Orphaned Block Slide.png'
 
+// korean images
+import blockHeaderKr from '../img/resources/korean/Encyclopedia - Block Header Slide - KR.png'
+import blockHeightKr from '../img/resources/korean/Encyclopedia - Block Height Slide - KR.png'
+import blockKr from '../img/resources/korean/Encyclopedia - Block Slide - KR.png'
+import blockchainKr from '../img/resources/korean/Encyclopedia - Blockchain Slide - KR.png'
+import genesisBlockKr from '../img/resources/korean/Encyclopedia - Genesis Block Slide - KR.png'
+import mainChainKr from '../img/resources/korean/Encyclopedia - Main Chain Slide - KR.png'
+import merkleTreeKr from '../img/resources/korean/Encyclopedia - Merkle Tree Slide - KR.png'
+import orphanedBlockKr from '../img/resources/korean/Encyclopedia - Orphaned Block Slide - KR.png'
+
 import './resources.css'
 
 export class Resources extends React.Component {
-  render () {
-    const slidesInfo = [
+  state = {
+    language: 'english',
+    englargedSlide: undefined,
+    english: [
       {
         slide: blockchain,
         name: 'Blockchain',
@@ -39,32 +52,95 @@ export class Resources extends React.Component {
         slide: merkleTree,
         name: 'Merkle Tree',
       }
+    ],
+    korean: [
+      {
+        slide: blockchainKr,
+        name: '블록체인'
+      }, {
+        slide: blockKr,
+        name: '블록'
+      }, {
+        slide: genesisBlockKr,
+        name: '제네시스 블록'
+      }, {
+        slide: blockHeaderKr,
+        name: '블록 헤더'
+      }, {
+        slide: blockHeightKr,
+        name: '블록 높이'
+      }, {
+        slide: mainChainKr,
+        name: '주요 체인'
+      }, {
+        slide: orphanedBlockKr,
+        name: '고아 블록'
+      }, {
+        slide: merkleTreeKr,
+        name: '머클 트리'
+      }
     ]
+  }
 
-    const slideComponents = slidesInfo.map((slide, index) => {
+  changeLanguage = language => {
+    this.setState({language})
+  }
+
+  enlargeSlide = slide => {
+    this.setState({enlargedSlide: slide})
+  }
+
+  closeSlide = () => {
+    this.setState({enlargedSlide: undefined})
+  }
+
+  render () {
+    const slides = this.state.language === 'english' ? this.state.english : this.state.korean
+
+    const slideComponents = slides.map((slide, index) => {
       return (
         <div key={index} className="slide">
-          <img src={slide.slide} alt={slide.name}/>
+          <img src={slide.slide} alt={slide.name} onClick={this.enlargeSlide.bind(null, slide)}/>
           <div className="name">{slide.name}</div>
         </div>
       )
     })
 
+    const enlargedSlide = this.state.enlargedSlide
+    const modal = (
+      <div>
+        <div className="modal" onClick={() => this.closeSlide()}></div>
+        <div className="enlarged-slide">
+          <img src={typeof(enlargedSlide) !== 'undefined' && enlargedSlide.slide} alt={typeof(enlargedSlide) !== 'undefined' && enlargedSlide.name}/>
+          <div className="close-button" onClick={() => this.closeSlide()}>&times;</div>
+        </div>
+      </div>
+    )
+
     return (
       <div className="resources-page">
+        {this.state.enlargedSlide && modal}
         <div className="resources-search">
           <div className="resources">
             <h2>Resources</h2>
             <p><strong>Are you new to cryptocurrency?</strong> This channel has you covered. You can navigate all the
               terms using the term search bar (to the left). Our team takes complicated topics and breaks them down into
               easy-to-digest infographics and illustrations. Enjoy and explore!</p>
-            <p><em>This resource page was created by @kr-marketing in collaboration with The Creative Crypto
-              Magazine.</em></p>
           </div>
           <div className="search">
-            <h2>Search a Term</h2>
-            {/* search bar here */}
-            {/* toggle buttons here */}
+            <div className="language-buttons">
+              <button
+                className={`language-button ${this.state.language === 'english' && 'language-button-active'}`}
+                onClick={(() => this.changeLanguage('english'))}>EN
+              </button>
+              <button
+                className={`language-button ${this.state.language === 'korean' && 'language-button-active'}`}
+                onClick={(() => this.changeLanguage('korean'))}>KR
+              </button>
+              <div className="toggle-message">Toggle the language icons to alternate English and Korean.</div>
+            </div>
+            <p><em>This resource page was created by @kr-marketing in collaboration with The Creative Crypto
+              Magazine.</em></p>
           </div>
         </div>
         <div className="encyclopedia">
